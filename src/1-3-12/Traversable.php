5318097e497a1a29e18ff594ffb4d5c315059bee
sergeev <sergeev.v.v@effective-group.ru>
2023-05-05 15:06:45 +0300

    PHP SPL, Predefined Interfaces & Classes

---
+++ src/1-3-12/Traversable.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Нельзя реализовать, вместо него нужно использовать либо IteratorAggregate, либо Iterator
+class MyClass implements Traversable
+{
+
+}
+
+instanceof Traversable - проверяем, что класс
+*/
